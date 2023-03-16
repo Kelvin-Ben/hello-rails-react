f5f7b944bb50e991719160775c5dbf19644e7e32
Kelvin-Ben <kevoben14@gmail.com>
2023-03-16 22:32:39 +0300

    Render the app component to the static page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,26 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Greeting from './components/Greeting';
+import store from './redux/store';
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Route exact path="/" component={Greeting} />
+      </BrowserRouter>
+    </Provider>
+  );
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <App />,
+    document.getElementById('root'),
+  );
+});
