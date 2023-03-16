e130dbe64da02b280bab0082c51aa17168116962
Kelvin-Ben <kevoben14@gmail.com>
2023-03-16 22:33:09 +0300

    Create routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root 'messages#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+    resources :messages
 end
